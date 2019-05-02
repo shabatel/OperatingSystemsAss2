b762e74023640ce95aad4108d73189abbaac77b7
shabatel <shabatel@post.bgu.ac.il>
2019-05-02 20:07:59 +0300

    end of mutex baby

--- syscall.c
+++ syscall.c
@@ -108,6 +108,10 @@ extern int sys_kthread_id(void);
 extern int sys_kthread_exit(void);
 extern int sys_kthread_join(void);
 extern int sys_kthread_create(void);
+extern int sys_kthread_mutex_alloc(void);
+extern int sys_kthread_mutex_dealloc(void);
+extern int sys_kthread_mutex_lock(void);
+extern int sys_kthread_mutex_unlock(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -135,6 +139,10 @@ static int (*syscalls[])(void) = {
 [SYS_kthread_exit]   sys_kthread_exit,
 [SYS_kthread_join]   sys_kthread_join,
 [SYS_kthread_create]   sys_kthread_create,
+[SYS_kthread_mutex_alloc]   sys_kthread_mutex_alloc,
+[SYS_kthread_mutex_dealloc]   sys_kthread_mutex_dealloc,
+[SYS_kthread_mutex_lock]  sys_kthread_mutex_lock,
+[SYS_kthread_mutex_unlock]  sys_kthread_mutex_unlock,
 };
 
 void
